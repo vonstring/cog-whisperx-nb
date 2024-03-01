ba6fac1330cb86a90d44fb5fa34d57edde5263df
Kristian von Streng Hæhre <kristian.vsh@gmail.com>
2024-03-01 17:54:42 +0100

    Added options to output/srt and to control line width/count

--- predict.py
+++ predict.py
@@ -6,38 +6,37 @@ os.environ['TORCH_HOME'] = '/src/torch_models'
 from cog import BasePredictor, Input, Path
 import torch
 import whisperx
+from whisperx.utils import get_writer
 import json
-
+import io
 
 compute_type="float16"
 class Predictor(BasePredictor):
     def setup(self):
         """Load the model into memory to make running multiple predictions efficient"""
         self.device = "cuda"
-        self.model = whisperx.load_model("NbAiLabBeta/nb-whisper-large", self.device, compute_type=compute_type)
-        self.alignment_model = None
-        self.metadata = None
-        self.alignment_language = None
+        self.model = whisperx.load_model("NbAiLabBeta/nb-whisper-large", self.device, language="no", compute_type=compute_type)
+        self.alignment_model, self.metadata = whisperx.load_align_model(language_code="no", device=self.device)
 
     def predict(
         self,
         audio: Path = Input(description="Audio file"),
         batch_size: int = Input(description="Parallelization of input audio transcription", default=32),
         align_output: bool = Input(description="Use if you need word-level timing and not just batched transcription", default=False),
-        only_text: bool = Input(description="Set if you only want to return text; otherwise, segment metadata will be returned as well.", default=False),
-        language: str = Input(description="Language code for the audio file", default="en"),
+        only_text: bool = Input(description="Only return the text of the transcription", default=False),
+        output_srt: bool = Input(description="Output in SRT format", default=False),
+        output_vtt: bool = Input(description="Output in VTT format", default=False),
+        max_line_width: int = Input(description="Maximum line width for SRT and VTT output", default=45),
+        max_line_count: int = Input(description="Maximum line count for SRT and VTT output", default=2),
+        highlight_words: bool = Input(description="Visualise word timings in SRT ouutput", default=False),
         debug: bool = Input(description="Print out memory usage information.", default=False)
     ) -> str:
         """Run a single prediction on the model"""
         with torch.inference_mode():
-            result = self.model.transcribe(str(audio), batch_size=batch_size, language=language)
+            result = self.model.transcribe(str(audio), batch_size=batch_size, language="no")
             # result is dict w/keys ['segments', 'language']
             # segments is a list of dicts,each dict has {'text': <text>, 'start': <start_time_msec>, 'end': <end_time_msec> }
             if align_output:
-                if self.alignment_language != result['language']:
-                    self.alignment_language = result['language']
-                    self.alignment_model, self.metadata = whisperx.load_alignment_model(language_code=result['language'], device=self.device)
-
                 # NOTE - the "only_text" flag makes no sense with this flag, but we'll do it anyway
                 result = whisperx.align(result['segments'], self.alignment_model, self.metadata, str(audio), self.device, return_char_alignments=False)
                 # dict w/keys ['segments', 'word_segments']
@@ -46,7 +45,28 @@ class Predictor(BasePredictor):
                 # aligned_result['segments'] - same as result segments, but w/a ['words'] segment which contains timing information above. 
                 # return_char_alignments adds in character level alignments. it is: too many. 
             if only_text:
-                return ''.join([val.text for val in result['segments']])
+                return ''.join([val['text'] for val in result['segments']])
+
+            def format_subtitles(format):
+                f = io.StringIO()
+                writer = get_writer(format, None)
+                writer.write_result(result, f, {
+                    "max_line_width": max_line_width,
+                    "max_line_count": max_line_count,
+                    "highlight_words": highlight_words
+                })
+                ret = f.getvalue()
+                f.close()
+                return ret
+
+            if output_srt or output_vtt:
+                ret = {}
+                result['language'] = 'no'
+                if output_srt:
+                    ret['srt'] = format_subtitles('srt')
+                if output_vtt:
+                    ret['vtt'] = format_subtitles('vtt')
+                return json.dumps(ret)
             if debug:
                 print(f"max gpu memory allocated over runtime: {torch.cuda.max_memory_reserved() / (1024 ** 3):.2f} GB")
         return json.dumps(result['segments'])
